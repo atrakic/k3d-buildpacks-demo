90918124647cd23332e3f517660d7860a86ae674
Admir Trakic <atrakic@users.noreply.github.com>
2021-07-21 15:28:26 +0200

    Generate k8s yaml manifest

---
+++ scripts/generate.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+[[ -n $DEBUG ]] && set -x
+
+APP=${1:-demo}
+IMAGE=myproj/demo:v1.0.0
+
+main() {
+  local file=manifests/deploy.yml
+
+  echo --- > $file
+  kubectl create service clusterip $APP --tcp=80:8080 --dry-run=client -o yaml >> $file
+
+  echo --- >> $file
+  kubectl create deployment $APP --image $IMAGE --dry-run=client -o yaml | grep -Ev status >> $file
+
+  [[ -z $NO_INGRESS ]] && cat >> $file <<EOF
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: $APP
+  annotations:
+    ingress.kubernetes.io/ssl-redirect: "false"
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: $APP
+            port:
+              number: 80
+EOF
+}
+
+main "$@"
